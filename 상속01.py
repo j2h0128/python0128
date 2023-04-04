3a1ed4195d19040e7108058086083a8a723c0a7b
j2h0128 <jangho64@gmail.com>
2023-04-04 13:23:02 +0900

    상속연습_01

--- 상속01.py
+++ 상속01.py
@@ -1,3 +1,4 @@
+#부모 클래스 정의(Super Class)
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
@@ -6,17 +7,27 @@ class Person:
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
 
+#자식 클래스 정의(Sub Class): 대학생
 class Student(Person):
+    #상속받은 매서드 덮어쓰기(재정의, overfide)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #부모 생성자 호출
+        #self.name = name
+        #self.phoneNumber = phoneNumber
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
 
+    #상속받고 덮어쓰기
+    def printInfo(self):
+        print("Info(이름:{0}, 전공:{1}, 학번: {2})".format(self.name, self.subject, self.studentID))
+        print("Info(이름:{0}, 전공:{1}, 학번: {2})".format(self.name, self.subject, self.studentID))
 
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
+#print(p.__dict__)
+#print(s.__dict__)
 
+p.printInfo()
+s.printInfo()
 
--- 생성자소멸자01.py
+++ 생성자소멸자01.py
@@ -1,8 +1,16 @@
 # -*- 생성자와 소멸자 -*-
 class MyClass:
+    #초기화(생성자매소드)
     def __init__(self, value):
         self.value = value
         print("Instace is created! value = ", value)
+        #소멸자매소드
     def __del__(self):
         print("Instance is deleted!")
 
+#인스턴스 생성
+m1=MyClass(5)
+#메모리관리 자동
+#del m1 --> 이건 굳이 안 해도 됨
+
+print("전체 코드 실행 종료")
